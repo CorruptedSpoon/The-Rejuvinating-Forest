3c571916e4a24e40315011d3e65c453c649da719
Nicholas Jones <nxj8268@g.rit.edu>
2021-10-01 17:20:09 -0400

    explorer stuff
    Trying to get explorer spawn working, I've edited the schedule and added placeholder dialogue and such. Set up asset folder for the explorer as well, didn't get him to actually spawn in yet, documentation isn't great and there isn't much help out there not using content patcher mod

--- RejuvenatingForest/ModEntry.cs
+++ RejuvenatingForest/ModEntry.cs
@@ -8,7 +8,7 @@ using StardewValley;
 namespace RejuvenatingForest
 {
     /// <summary>The mod entry point.</summary>
-    public class ModEntry : Mod
+    public class ModEntry : Mod, IAssetLoader
     {
         /*********
         ** Public methods
@@ -18,7 +18,32 @@ namespace RejuvenatingForest
         public override void Entry(IModHelper helper)
         {
             helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            //trying to get explorer npc to spawn, I have all the files created but getting him to spawn through code is proving, difficult
+            //only information I can find that's of use is using the content patcher mod so it's just going to take a bunch more digging in the 
+            //documentation I guess, which is kinda not too helpful but, yeah...
+            //AnimatedSprite sprite = helper.Content.Load<AnimatedSprite>("assets/ExplorerPlaceholder.png", ContentSource.ModFolder);
+            //NPC explorer(sprite, Vector2(20,20), 1, "Explorer", null);
+            //Game1.getLocationFromName("Town").addCharacter(explorer);
         }
+
+        /*
+        //trying to add explorer npc things
+        public bool CanLoad<T>(IAssetInfo asset)
+        {
+            return asset.AssetNameEquals("Characters/Dialogue/Explorer");
+        }
+
+        /// <summary>Load a matched asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public T Load<T>(IAssetInfo asset)
+        {
+            return (T)(object)new Dictionary<string, string> // (T)(object) converts a known type to the generic 'T' placeholder
+            {
+                ["Introduction"] = "Hi there! My name is the Explorer."
+            };
+        }
+        */
+
 
 
         /*********
