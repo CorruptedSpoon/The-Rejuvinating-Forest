47859bb6860a2394bfc8de3e0dc7d03b7870f913
Nicholas Jones <nxj8268@g.rit.edu>
2021-11-15 15:07:08 -0500

    warp things, they are in forest above explorer

--- RejuvenatingForest/ModEntry.cs
+++ RejuvenatingForest/ModEntry.cs
@@ -18,10 +18,6 @@ namespace RejuvenatingForest
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod, IAssetEditor, IAssetLoader
     {
-        IDictionary<string, string> explorerDialogue;
-        IDictionary<string, string> explorerSchedule;
-        Texture2D explorerSprite;
-        Texture2D explorerPortrait;
         /*********
         ** Public methods
         *********/
@@ -39,7 +35,7 @@ namespace RejuvenatingForest
         {
             if (asset.AssetNameEquals("Maps/Forest"))
                 return true;
-            if (asset.AssetNameEquals("Maps/Beach"))
+            if (asset.AssetNameEquals("Maps/Town"))
                 return true;
             return false;
         }
@@ -47,12 +43,14 @@ namespace RejuvenatingForest
         {
             if (asset.AssetNameEquals("Maps/Forest"))
             {
-                asset.AsMap().Data.Properties["Warp"] += " 4 4 Beach 28 15";
+                string warps = asset.AsMap().Data.Properties["Warp"];
+                asset.AsMap().Data.Properties["Warp"] = $"4 4 Town 16 30 {warps}";
                 asset.AsMap().Data.Properties["Warp"] += " 3 4 RejuvenatingForest 94 31";
             }
-            if (asset.AssetNameEquals("Maps/Beach"))
+            if (asset.AssetNameEquals("Maps/Town"))
             {
-                asset.AsMap().Data.Properties["Warp"] += " 26 15 Forest 5 5";
+                string warps = asset.AsMap().Data.Properties["Warp"];
+                asset.AsMap().Data.Properties["Warp"] = $"18 30 Forest 5 5 {warps}";
             }
         }
 
@@ -107,47 +105,7 @@ namespace RejuvenatingForest
             xTile.Map busStop = Game1.getLocationFromName("BusStop").Map;
 
 
-            //xTile.Map forest = Game1.getLocationFromName("Forest").Map;
-            //forest.Properties.TryGetValue("Warp", out xTile.ObjectModel.PropertyValue initialWarps);
-            //forest.Properties.Remove("Warp");
-            //forest.Properties.Add("Warp", initialWarps + " 6 7 Beach 31 15");
-            ////forest.Properties.Add("Warp", "6 7 Beach 31 15");
-
-
-            //xTile.Map RejuvForest = Game1.getLocationFromName("Beach").Map;
-            //RejuvForest.Properties.TryGetValue("Warp", out xTile.ObjectModel.PropertyValue rejuvWarps);
-            //RejuvForest.Properties.Remove("Warp");
-            //RejuvForest.Properties.Add("Warp", rejuvWarps + " 30 15 Forest 5 7");
-            //RejuvForest.Properties.Add("Warp", "30 15 Forest 5 7");
-
-            /*
-            GameLocation forest = Game1.getLocationFromName("Forest");
-            GameLocation rejuv = Game1.getLocationFromName("RejuvenatingForest");
-            GameLocation bus = Game1.getLocationFromName("BusStop");
-            GameLocation beach = Game1.getLocationFromName("Beach");
-
-            forest.map.Properties["Warp"] += " 4 4 Beach 28 15";
-            beach.map.Properties["Warp"] += " 26 15 Forest 4 5";
-            forest.map.Properties["Warp"] += " 3 4 RejuvenatingForest 94 31";
-            rejuv.map.Properties["Warp"] += " 96 32 Forest 3 5";
-
-            forest.updateWarps();
-            rejuv.updateWarps();
-            beach.updateWarps();*/
-            GameLocation rejuv = Game1.getLocationFromName("RejuvenatingForest");
-            rejuv.map.Properties["Warp"] += " 96 32 Forest 3 5";
-            rejuv.updateWarps();
             NPC.populateRoutesFromLocationToLocationList();
-            
-            
-            //GameLocation
-            //forest.setTileProperty(6, 7, "Building", "Warp", "6 7 Beach 31 15");
-            //beach.setTileProperty(28, 15, "Building", "Warp", "28 15 Forest 5 7");
-
-            //GameLocation forest = Game1.getLocationFromName("Forest");
-            //Layer forestLayer = forest.map.GetLayer("Back");
-            //Tile warpTile = forestLayer.Tiles[6, 7];
-            //warpTile.Properties["@NPCWarp"] = "6 7 Town 32 64";
         }
 
         private void OnDayStart(object sender, DayStartedEventArgs e)
