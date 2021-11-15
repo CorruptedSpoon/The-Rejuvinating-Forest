c972047a5f33d3f3dbd8d4d9d764ebc876661fa3
Nicholas Jones <nxj8268@g.rit.edu>
2021-11-15 13:41:56 -0500

    idk things for me to push

--- RejuvenatingForest/ModEntry.cs
+++ RejuvenatingForest/ModEntry.cs
@@ -16,7 +16,7 @@ using xTile.ObjectModel;
 namespace RejuvenatingForest
 {
     /// <summary>The mod entry point.</summary>
-    public class ModEntry : Mod//, IAssetLoader, IAssetEditor
+    public class ModEntry : Mod, IAssetEditor //, IAssetLoader, 
     {
         IDictionary<string, string> explorerDialogue;
         IDictionary<string, string> explorerSchedule;
@@ -25,6 +25,28 @@ namespace RejuvenatingForest
         /*********
         ** Public methods
         *********/
+
+        public bool CanEdit<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Maps/Forest"))
+                return true;
+            if (asset.AssetNameEquals("Maps/Beach"))
+                return true;
+            return false;
+        }
+        public void Edit<T>(IAssetData asset)
+        {
+            if (asset.AssetNameEquals("Maps/Forest"))
+            {
+                asset.AsMap().Data.Properties["Warp"] += " 4 4 Beach 28 15";
+                asset.AsMap().Data.Properties["Warp"] += " 3 4 RejuvenatingForest 94 31";
+            }
+            if (asset.AssetNameEquals("Maps/Beach"))
+            {
+                asset.AsMap().Data.Properties["Warp"] += " 26 15 Forest 5 5";
+            }
+        }
+
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
@@ -88,15 +110,24 @@ namespace RejuvenatingForest
             //RejuvForest.Properties.Remove("Warp");
             //RejuvForest.Properties.Add("Warp", rejuvWarps + " 30 15 Forest 5 7");
             //RejuvForest.Properties.Add("Warp", "30 15 Forest 5 7");
-            GameLocation forest = Game1.getLocationFromName("Forest");
-            GameLocation beach = Game1.getLocationFromName("RejuvenatingForest");
-            GameLocation bus = Game1.getLocationFromName("BusStop");
 
-            forest.map.Properties["Warp"] += " 3 3 RejuvenatingForest 92 31";
-            beach.map.Properties["Warp"] += " 93 31 Forest 3 5";
+            /*
+            GameLocation forest = Game1.getLocationFromName("Forest");
+            GameLocation rejuv = Game1.getLocationFromName("RejuvenatingForest");
+            GameLocation bus = Game1.getLocationFromName("BusStop");
+            GameLocation beach = Game1.getLocationFromName("Beach");
+
+            forest.map.Properties["Warp"] += " 4 4 Beach 28 15";
+            beach.map.Properties["Warp"] += " 26 15 Forest 4 5";
+            forest.map.Properties["Warp"] += " 3 4 RejuvenatingForest 94 31";
+            rejuv.map.Properties["Warp"] += " 96 32 Forest 3 5";
 
             forest.updateWarps();
-            beach.updateWarps();
+            rejuv.updateWarps();
+            beach.updateWarps();*/
+            NPC.populateRoutesFromLocationToLocationList();
+            
+            
             //GameLocation
             //forest.setTileProperty(6, 7, "Building", "Warp", "6 7 Beach 31 15");
             //beach.setTileProperty(28, 15, "Building", "Warp", "28 15 Forest 5 7");
